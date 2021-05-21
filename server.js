62225407fb449d86c4aa985a6c480f0af842c462
bobbyling <bobbyling@hitachi-ebworx.com>
2021-05-22 00:53:59 +0800

    dev-api: Remove static file and article from server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const config = require('config')
 const history = require('connect-history-api-fallback')
-const Article = require('./models/Article')
-const articleRouter = require('./routes/articles')
 const apiRouter = require('./routes/api')
 
 console.debug('config: ', config.get('app'), config.get('db'))
@@ -26,26 +24,20 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(methodOverride('_method'))
 
-app.get('/', async (req, res) => {
-	const articles = await Article.find().sort({ createdAt: 'desc' })
-	res.render('articles/index', { articles })
-})
-
-app.use('/articles', articleRouter)
 app.use('/api', apiRouter)
 
 // ====== Setup SPA static folder ========
-const staticFileMiddleware = express.static('static/www')
-app.use(staticFileMiddleware)
-app.use(
-	history({
-		disableDotRule: true,
-		index: '/index.html',
-		verbose: true,
-		logger: console.log.bind(console)
-	})
-)
-app.use(staticFileMiddleware)
+// const staticFileMiddleware = express.static('static/www')
+// app.use(staticFileMiddleware)
+// app.use(
+// 	history({
+// 		disableDotRule: true,
+// 		index: '/index.html',
+// 		verbose: true,
+// 		logger: console.log.bind(console)
+// 	})
+// )
+// app.use(staticFileMiddleware)
 // ========================================
 
 app.listen(_appPort, () => {
