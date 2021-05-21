da2d763629cb0f14ab67b8d1c5dcfcfabcc5b133
bobbyling <bobbyling@hitachi-ebworx.com>
2021-05-22 00:50:10 +0800

    dev-api: change shipRegistry model

--- models/ShipRegistry.js
+++ models/ShipRegistry.js
@@ -18,7 +18,6 @@ const shipRegistrySchema = mongoose.Schema(
 		class: String,
 		portOfRegistry: String,
 		statusOfVessel: String,
-		new_field: String,
 		characteristics: Object,
 		specialCharacteristics: Object
 	},
@@ -26,15 +25,6 @@ const shipRegistrySchema = mongoose.Schema(
 		timestamps: true
 	}
 )
-
-// shipRegistrySchema.pre('validate', function validate() {
-// if (this.title) {
-// 	this.slug = slugify(this.title, { lower: true, strict: true })
-// }
-// if (this.markdown) {
-// 	this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
-// }
-// })
 
 const ShipRegistry = mongoose.model('ShipRegistry', shipRegistrySchema)
 ShipRegistry.get = (callback, limit) => {
@@ -60,11 +50,6 @@ module.exports = {
 		'portOfRegistry',
 		'statusOfVessel',
 		'characteristics',
-		'new_field',
 		'specialCharacteristics'
 	]
 }
-
-// module.exports.get = (callback, limit) => {
-// 	Contact.find(callback).limit(limit)
-// }
