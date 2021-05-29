c51a0b477ed4014643025a7fb40ead22be6450d1
bobbyling <bobbyling@hitachi-ebworx.com>
2021-05-30 01:13:52 +0800

    dev-api: remove credentials for db connection

--- server.js
+++ server.js
@@ -5,41 +5,68 @@ const config = require('config')
 const history = require('connect-history-api-fallback')
 const apiRouter = require('./routes/api')
 
-console.debug('config: ', config.get('app'), config.get('db'))
-const _appPort = process.env.PORT || config.get('app.port')
-const _dbUrl = config.get('db.url')
+require('dotenv').config({ path: `${__dirname}/.env.local` })
 
+console.debug('** config->', config)
+
+const APP_PORT = process.env.PORT || config.get('app.port')
+const DB_URL = config.get('db.url')
+const DB_USERNAME = process.env.MONGODB_USERNAME
+const DB_PASSWORD = process.env.MONGODB_PASSWORD
+let connectRetry = config.get('db.connectRetry') || 3
+
+const self = this
 const app = express()
 
-mongoose.connect(_dbUrl, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-	useCreateIndex: true
-})
+self.connectToDB = async () => {
+	try {
+		await mongoose.connect(DB_URL, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			user: DB_USERNAME,
+			pass: DB_PASSWORD
+		})
+	} catch (error) {
+		console.error(error)
+		console.warn('** Connect Retry left -> ', connectRetry)
+		connectRetry -= 1
+		if (error && connectRetry > 0) {
+			await self.connectToDB()
+		} else {
+			throw error
+		}
+	}
+}
 
-app.set('view engine', 'ejs')
-app.set('port', _appPort)
+self.initalize = async () => {
+	await self.connectToDB()
+	app.set('view engine', 'ejs')
+	app.set('port', APP_PORT)
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(methodOverride('_method'))
+	app.use(express.urlencoded({ extended: false }))
+	app.use(express.json())
+	app.use(methodOverride('_method'))
 
-app.use('/api', apiRouter)
+	app.use('/api', apiRouter)
 
-// ====== Setup SPA static folder ========
-// const staticFileMiddleware = express.static('static/www')
-// app.use(staticFileMiddleware)
-// app.use(
-// 	history({
-// 		disableDotRule: true,
-// 		index: '/index.html',
-// 		verbose: true,
-// 		logger: console.log.bind(console)
-// 	})
-// )
-// app.use(staticFileMiddleware)
-// ========================================
+	// ====== Setup SPA static folder ========
+	// const staticFileMiddleware = express.static('static/www')
+	// app.use(staticFileMiddleware)
+	// app.use(
+	// 	history({
+	// 		disableDotRule: true,
+	// 		index: '/index.html',
+	// 		verbose: true,
+	// 		logger: console.log.bind(console)
+	// 	})
+	// )
+	// app.use(staticFileMiddleware)
+	// ========================================
 
-app.listen(_appPort, () => {
-	console.debug(`App started at port ${_appPort}`)
-})
+	app.listen(APP_PORT, () => {
+		console.debug(`** App started at port ${APP_PORT}`)
+	})
+}
+
+self.initalize()
